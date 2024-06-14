d3cb943f89e3c7de7c0697a467836142a4871fc8
laith-abinajem <laithnuman2018@gmail.com>
2024-06-14 18:36:48 +0300

    chnages

--- resources/views/dashboard/pages/subscription/index.blade.php
+++ resources/views/dashboard/pages/subscription/index.blade.php
@@ -175,7 +175,10 @@
                             </div>
                         </div>
                         <!-- add else -->
-                        <!-- add (Card form) -->
+                        <div id="form-container">
+                            <div id="card-container"></div>
+                            <button class="button btn btn-primary me-3" id="card-button">Save Card</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -396,24 +399,66 @@
         </div>
     </div>
 </div>
+<script src="https://sandbox.web.squarecdn.com/v1/square.js"></script>
+<script>
+    async function initializeSquarePayments() {
+        const payments = Square.payments('sandbox-sq0idb-YUWObSyjVpaI6A4yV6iX9A', 'sandbox'); // or 'production'
+        const card = await payments.card();
+        await card.attach('#card-container');
+        const customerId = @json(auth()->user()->square_customer_id);
+        document.getElementById('card-button').addEventListener('click', async () => {
+            const { token, error } = await card.tokenize();
+            if (error) {
+                console.error(error);
+                return;
+            }
+            console.log(token)
+            const response = await fetch('/processPayment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': '{{ csrf_token() }}',
+                },
+                body: JSON.stringify({
+                    nonce: token,
+                    customer_id:customerId
+                })
+            });
+
+            if (response.ok) {
+                const cardData = await response.json();
+                console.log('Card added:', cardData);
+            } else {
+                const errorData = await response.json();
+                console.error('Error adding card:', errorData);
+            }
+        });
+    }
+
+    initializeSquarePayments();
+</script>
 <script>
     document.getElementById('payment-button').addEventListener('click', function() {
         document.getElementById('subscription-form').action = "{{ route('dashboard.process-payment') }}";
     });
-
-    document.getElementById('trial-button').addEventListener('click', function() {
-        document.getElementById('subscription-form').action = "{{ route('dashboard.subscription.store') }}";
-    });
+    var trial_btn = document.getElementById('trial-button');
+    if(trial_btn){
+        trial_btn.addEventListener('click', function() {
+            document.getElementById('subscription-form').action = "{{ route('dashboard.subscription.store') }}";
+        });
+    }
+   
 </script>
 <script>
     document.getElementById('payment-button2').addEventListener('click', function() {
         document.getElementById('subscription-form2').action = "{{ route('dashboard.process-payment') }}";
     });
-
-    document.getElementById('trial-button2').addEventListener('click', function() {
-        document.getElementById('subscription-form2').action = "{{ route('dashboard.subscription.store') }}";
-    });
-
+    var trial_btn2 = document.getElementById('trial-button2');
+    if(trial_btn2){
+        trial_btn2.addEventListener('click', function() {
+            document.getElementById('subscription-form2').action = "{{ route('dashboard.subscription.store') }}";
+        });
+    }
     document.addEventListener('DOMContentLoaded', function() {
     var managePlanModal = document.getElementById('managePlanModal');
 
