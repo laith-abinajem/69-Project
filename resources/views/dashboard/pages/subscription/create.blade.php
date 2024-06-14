7316aa15405e1a1a9c1af52c97a9fbaea4bd1d66
laith-abinajem <laithnuman2018@gmail.com>
2024-06-14 15:49:35 +0300

    changes

--- resources/views/dashboard/pages/subscription/create.blade.php
+++ resources/views/dashboard/pages/subscription/create.blade.php
@@ -5,39 +5,35 @@
 
 <script src="https://sandbox.web.squarecdn.com/v1/square.js"></script>
 <script>
-    async function initializeSquarePayments() {
-        const payments = Square.payments('sq0idp-8mKT7to57PX7XoTmEAiTPA', 'production'); // or 'production'
-        const card = await payments.card();
-        await card.attach('#card-container');
+    const payments = Square.payments('sandbox-sq0idb-YUWObSyjVpaI6A4yV6iX9A', 'sandbox'); // or 'production'
+    const card = await payments.card();
+    await card.attach('#card-container');
 
-        document.getElementById('card-button').addEventListener('click', async () => {
-            const { token, error } = await card.tokenize();
-            if (error) {
-                console.error(error);
-                return;
-            }
+    document.getElementById('card-button').addEventListener('click', async () => {
+        const { token, error } = await card.tokenize();
+        if (error) {
+            console.error(error);
+            return;
+        }
 
-            const response = await fetch('/processPayment', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-Token': '{{ csrf_token() }}',
-                },
-                body: JSON.stringify({
-                    nonce: token,
-                    customer_id: '5H6FNDRBN0DTQ40CKPAAPTGM1G'
-                })
-            });
-
-            if (response.ok) {
-                const cardData = await response.json();
-                console.log('Card added:', cardData);
-            } else {
-                const errorData = await response.json();
-                console.error('Error adding card:', errorData);
-            }
+        const response = await fetch('/add-card', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-Token': '{{ csrf_token() }}',
+            },
+            body: JSON.stringify({
+                nonce: token,
+                customer_id: 'your-customer-id'
+            })
         });
-    }
 
-    initializeSquarePayments();
+        if (response.ok) {
+            const cardData = await response.json();
+            console.log('Card added:', cardData);
+        } else {
+            const errorData = await response.json();
+            console.error('Error adding card:', errorData);
+        }
+    });
 </script>
