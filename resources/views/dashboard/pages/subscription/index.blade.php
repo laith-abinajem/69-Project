2162ebd3773abca1e346d4e8179ba3e7cc1565f7
laith-abinajem <laithnuman2018@gmail.com>
2024-06-19 18:48:53 +0300

    chanegs

--- resources/views/dashboard/pages/subscription/index.blade.php
+++ resources/views/dashboard/pages/subscription/index.blade.php
@@ -540,10 +540,10 @@
         </div>
     </div>
 </div>
-<script src="https://sandbox.web.squarecdn.com/v1/square.js"></script>
+<script src="https://web.squarecdn.com/v1/square.js"></script>
 <script>
     async function initializeSquarePayments() {
-        const payments = Square.payments('sq0idp-8mKT7to57PX7XoTmEAiTPA', 'sandbox'); // or 'production'
+        const payments = Square.payments('sq0idp-8mKT7to57PX7XoTmEAiTPA', 'production'); // or 'production'
         const card = await payments.card();
         await card.attach('#card-container');
         const customerId = @json(auth()->user()->square_customer_id);
