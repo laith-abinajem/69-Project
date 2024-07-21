5f067755fa9047121ccd56ceee47baf3b1bf91d6
laith-abinajem <laithnuman2018@gmail.com>
2024-07-21 21:09:01 +0300

    chnages

--- resources/views/dashboard/pages/light/edit.blade.php
+++ resources/views/dashboard/pages/light/edit.blade.php
@@ -86,7 +86,7 @@
                                                 @foreach($subclass as $subclass_index => $subclass_item)
                                                     @php
                                                         $details = $lightTint->lightDetails->where('class_car', $class_item)->where('sub_class_car', $subclass_item);
-                                                        $detail = $tintBrand->lightDetails
+                                                        $detail = $lightTint->lightDetails
                                                         ->where('class_car', $class_item)
                                                         ->where('sub_class_car', $subclass_item)
                                                         ->first();
